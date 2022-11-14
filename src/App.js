3a858e6726ecc6b5f1675f9c6d68aea47de4a878
TracyOakley <tloak12@gmail.com>
2022-11-14 00:33:40 +0000

    Completed Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      city:''
+    }
+  }
+
+
+handleSubmit=e=>{
+  e.preventDefault();
+  // console.log(e.target.city.value);
+    // // we can't put things in state and expect to use them right away, so this won't work:
+    // this.setState({
+    //   city: e.target.city.value
+    // });
+
+    //search for data about city
+    console.log(this.state.city);
+  }
+
+handleInputChange = e => {
+  this.setState({
+    city: e.target.value
+  });
+  // search for a city???? — Can't do it here because if I want to search for "Seattle" it may searchh for "S" "Se" and then "Sea" ...
+}
+
+render(){
+  console.log(this.state.city);
+  return(
+    <>
+        {/* Add a separate Header, Main and Footer components */}
+          {/* This is a useful example for future */}
+
+        <h1>City Explorer</h1>
+        <form onSubmit={this.handleSubmit}> 
+          <label>Search for a City
+          <input type="text" name="city" onChange={this.handleInputChange}/>
+          </label>
+          <button type="submit">Search for a City</button>
+        </form>
+      </>
   );
+}
 }
 
 export default App;
